72760663a79cf73bcfe97ac39ec81e25a5603a24
PawelGrab <pgrabczynski@poczta.onet.pl>
2018-06-06 21:30:56 +0200

    Lomuto algorithm

--- ctablica.cpp
+++ ctablica.cpp
@@ -9,7 +9,7 @@ std::pair<int, int> CTablica::bubble_sort()
 {
     int SwapCounter=0;
     int ComparisonCounter=0;
-    for(int i=0;i<tab.size();i++)
+    for(int i=0;i<tab.size();i++){
         for(int j=1;j<tab.size()-i;j++)
         {
             ComparisonCounter++;
@@ -19,7 +19,8 @@ std::pair<int, int> CTablica::bubble_sort()
                     SwapCounter++;
                 }
         }
-        return std::make_pair<int,int> (ComparisonCounter, SwapCounter);
+    }
+        return std::make_pair<int,int> (int (ComparisonCounter), int(SwapCounter));
 
     //std::pair <int, int> p;
     //return p;
@@ -27,9 +28,33 @@ std::pair<int, int> CTablica::bubble_sort()
     //return std::make_pair<int,int>(x,y);
 }
 
-std::pair<int, int> CTablica::quick_sort_lomuto()
+std::pair<int, int> CTablica::quick_sort_lomuto(int indexA, int indexB)
 {
+    std::cout<<indexA<<" "<<indexB<<std::endl;
 
+
+    int pivotValue, b;
+    int SwapCounter=0;
+    int ComparisonCounter=0;
+    pivotValue = tab[indexA];
+    std::swap (tab[indexA], tab[tab.size()-1]);
+    b = indexA;
+
+    for(int a =indexA; a <= tab.size()-1; a++) {
+        if(tab[a] <= pivotValue)
+            {
+            b++;
+
+            std::swap (tab[a], tab[b]);
+            }
+    }
+    std::swap(tab[b +1], tab[tab.size()-1]);
+
+    if(b-1>indexA)
+    quick_sort_lomuto(indexA,b-1 );
+    if(b-1<indexB)
+    quick_sort_lomuto(b+1, indexB);
+    return std::make_pair<int,int> (0,0);
 }
 
 std::pair<int, int> CTablica::quick_sort_hoare()
--- main.cpp
+++ main.cpp
@@ -5,12 +5,14 @@ using namespace std;
 int main()
 {
 
-    CSortTablica tab;
-    tab.interface();
 
     CTablica tab;
+    int indexA, indexB;
     tab.tab = {1, 2,3,};
     auto bubbleResult=tab.bubble_sort(); //auto: bubbleResult otrzymuje typ, ktory ma metoda po prawej stronie
     cout<<bubbleResult.first<<" "<<bubbleResult.second<<endl;
+    indexA=0;
+    indexB= tab.tab.size()-1;
+    tab.quick_sort_lomuto(indexA, indexB);
     return 0;
 }
